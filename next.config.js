85a6afcbd24e253530207e1bcc4014472615a1d4
warnigo <abubakirshavkatov0012@gmail.com>
2025-03-04 16:12:31 +0500

    fix: fix merge to new leads

--- next.config.js
+++ next.config.js
@@ -1,36 +1,19 @@
 const path = require('path')
 
 /** @type {import('next').NextConfig} */
-
-const withTM = require('next-transpile-modules')([
-  '@fullcalendar/common',
-  '@fullcalendar/react',
-  '@fullcalendar/daygrid',
-  '@fullcalendar/list',
-  '@fullcalendar/timegrid'
-])
-
-module.exports = withTM({
+module.exports = {
   trailingSlash: false,
 
   productionBrowserSourceMaps: false,
   optimizeFonts: false,
-  swcMinify: false,
+  swcMinify: true,
 
   webpack: (config, { dev }) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       apexcharts: path.resolve(__dirname, './node_modules/apexcharts-clevision')
     }
-    if (config.cache && !dev) {
-      config.cache = Object.freeze({
-        type: 'memory'
-      })
-    }
     return config
-  },
-  experimental: {
-    scrollRestoration: true
   },
   images: {
     remotePatterns: [
@@ -38,4 +21,4 @@ module.exports = withTM({
       { protocol: 'http', hostname: '*' }
     ]
   }
-})
+}
