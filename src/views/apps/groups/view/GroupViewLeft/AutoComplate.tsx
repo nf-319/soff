4352480a70ae7c372b50625c4bb1138dc7614abb
warnigo <abubakirshavkatov0012@gmail.com>
2025-03-15 12:46:48 +0500

    fix: fix create group drawer in time inputs

--- src/views/apps/groups/view/GroupViewLeft/AutoComplate.tsx
+++ src/views/apps/groups/view/GroupViewLeft/AutoComplate.tsx
@@ -1,12 +1,15 @@
+'use client'
+
 import TextField from '@mui/material/TextField'
 import Autocomplete, { AutocompleteChangeReason, AutocompleteChangeDetails } from '@mui/material/Autocomplete'
 import { useTranslation } from 'react-i18next'
-import { useEffect, useState } from 'react'
+import { SyntheticEvent, useEffect, useState } from 'react'
 import { StudentDetailType } from 'src/types/apps/studentsTypes'
-import useDebounce from 'src/hooks/useDebounce'
 import api from 'src/@core/utils/api'
 import { AutocompleteValue } from '@mui/material'
 import { FormikProps } from 'formik'
+import useDebounce from '../../../../../hooks/useDebounce'
+import { useRouter } from 'next/router'
 
 interface AutoCompleteProps {
   formik: FormikProps<{ student: number | null }>
@@ -14,42 +17,43 @@ interface AutoCompleteProps {
   selectedStudent: any
 }
 
-export default function AutoComplete({ formik, setSelectedStudents, selectedStudent }: AutoCompleteProps) {
+const AutoComplete = ({ formik, setSelectedStudents, selectedStudent }: AutoCompleteProps) => {
   const { t } = useTranslation()
   const [searchData, setSearchData] = useState<{ label: string; id: number }[]>([])
   const [search, setSearch] = useState('')
-  const [open, setOpen] = useState(false)
+  const debounceSearch = useDebounce(search, 400)
+  const router = useRouter()
+  const { id }  = router.query
 
   const searchStudent = async () => {
     setSearchData([])
-    const resp = await api.get('student/list/?search=' + search)
+    const resp = await api.get('student/list/', { params: { search: debounceSearch, group: id }})
     setSearchData(resp.data.results?.map((item: StudentDetailType) => ({ label: item?.first_name, id: item?.id })))
   }
 
   useEffect(() => {
     if (search !== '') {
-      searchStudent()
+      void searchStudent()
     } else {
       setSearchData([])
     }
-  }, [search])
+  }, [debounceSearch])
 
 
-    
+
   return (
     <Autocomplete
-    
-      open={search === '' || selectedStudent ? false : true}
-      // disablePortal
+      open={!(search === '' || selectedStudent)}
       onChange={(
-        event: React.SyntheticEvent,
+        event: SyntheticEvent,
         value: AutocompleteValue<{ label: string; id: number }, false, false, false>,
         reason: AutocompleteChangeReason,
         details?: AutocompleteChangeDetails<{ label: string; id: number }>
       ) => setSelectedStudents(value ? value.id : null)}
-      onInputChange={(event: React.SyntheticEvent, value: string) => setSearch(value)}
+      onInputChange={(event: SyntheticEvent, value: string) => setSearch(value)}
       id='combo-box-demo'
       options={searchData}
+      sx={{ border: '1px solid #00000' }}
       noOptionsText={"Ma'lumot yoq"}
       renderInput={params => (
         <TextField
@@ -62,3 +66,6 @@ export default function AutoComplete({ formik, setSelectedStudents, selectedStud
     />
   )
 }
+
+AutoComplete.displayName = 'AutoComplete'
+export default AutoComplete
