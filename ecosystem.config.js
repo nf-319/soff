189a5b3e2e297af79e0590535e8046d38b24db96
warnigo <abubakirshavkatov0012@gmail.com>
2025-04-16 21:15:10 +0500

    fix: fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,6 +16,23 @@ module.exports = {
       error: "./logs/error.log",
       log: "./logs/combined.log",
       time: true
+    },
+    {
+      name: "prod-subdomain-soffcrm-uz-3132",
+      script: "npm",
+      args: "run start",
+      interpreter: "/root/.nvm/versions/node/v20.10.0/bin/node",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production"
+      },
+      output: "./logs/out.log",
+      error: "./logs/error.log",
+      log: "./logs/combined.log",
+      time: true
     }
   ]
 };
