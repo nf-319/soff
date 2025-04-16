ef3360daa2b75dd6f79d40beb1f219fb545d7069
Otabek0880 <hdildora42@gmail.com>
2025-04-16 15:18:02 +0500

    debtors modal

--- src/views/apps/groups/view/GroupViewLeft/Delete.tsx
+++ src/views/apps/groups/view/GroupViewLeft/Delete.tsx
@@ -1,19 +1,38 @@
 import LoadingButton from '@mui/lab/LoadingButton'
-import { Button, Dialog, DialogActions, DialogContentText, DialogTitle } from '@mui/material'
+import {
+  Box,
+  Button,
+  Checkbox,
+  Chip,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  FormControlLabel,
+  FormGroup,
+  Typography
+} from '@mui/material'
+import { MoveRight } from 'lucide-react'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { toast } from 'react-hot-toast'
 import { useTranslation } from 'react-i18next'
 import { useAppDispatch, useAppSelector } from 'src/store'
-import { deleteGroup, handleEditClickOpen, setOpenDebtorsModal } from 'src/store/apps/groupDetails'
+import { deleteGroup, getStudents, handleEditClickOpen } from 'src/store/apps/groupDetails'
 
 export default function Delete() {
   const [isLoading, setLoading] = useState(false)
-  const { openEdit } = useAppSelector(state => state.groupDetails)
+  const { openEdit, students } = useAppSelector(state => state.groupDetails)
   const dispatch = useAppDispatch()
   const { t } = useTranslation()
   const { query, push } = useRouter()
+  const [resetBalance, setResetBalance] = useState(true)
 
+  const handleCheckboxChange = (event: any) => {
+    setResetBalance(event.target.checked)
+    console.log('Checkbox value:', event.target.checked)
+  }
   const handleDelete = async () => {
     setLoading(true)
     if (query.id) {
@@ -24,12 +43,19 @@ export default function Delete() {
       } else {
         toast.error(response.payload.msg || "Guruhni o'chirib bo'lmadi")
         if (response.payload.msg == "Guruhda barcha talabalar o'chirilmagan") {
-          dispatch(setOpenDebtorsModal(true))
         }
       }
     }
     setLoading(false)
   }
+
+  console.log(students)
+
+  useEffect(() => {
+    if (openEdit == 'delete') {
+      dispatch(getStudents({ id: query?.id, queryString: 'status=active,new' }))
+    }
+  }, [openEdit])
 
   return (
     <Dialog
@@ -43,6 +69,33 @@ export default function Delete() {
         {t("Guruhni o'chirishga rozimisiz?")}
       </DialogTitle>
       <DialogContentText sx={{ textAlign: 'center' }}>{t("O'chirilgan guruhlar arxivda saqlanadi")}</DialogContentText>
+      <DialogContent>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={resetBalance} onChange={handleCheckboxChange} />}
+            label="O'quvchi balansini 0 ga tushirish"
+          />
+        </FormGroup>
+        {students?.map((item: any) => (
+          <Box sx={{ display: 'flex', paddingY: 2, alignItems: 'center', justifyContent: 'space-between' }}>
+            <Typography>{item?.student.first_name}</Typography>
+            <Chip
+              variant='outlined'
+              color={item.student_group_balance >= 0 ? 'success' : 'error'}
+              label={
+                <span className='flex items-center gap-1'>
+                  {item?.student_group_balance} so'm{' '}
+                  {resetBalance && item.student_group_balance < 0 && (
+                    <span className='flex items-center gap-1'>
+                      <MoveRight size={12} /> 0 so'm
+                    </span>
+                  )}
+                </span>
+              }
+            />
+          </Box>
+        ))}
+      </DialogContent>
       <DialogActions sx={{ justifyContent: 'center' }}>
         <LoadingButton loading={isLoading} color='error' onClick={handleDelete} variant='outlined' sx={{ mr: 1 }}>
           {t("O'chirish")}
