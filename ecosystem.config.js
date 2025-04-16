27888ad46ee6b5af0bc275195f84837bbde5e41c
warnigo <abubakirshavkatov0012@gmail.com>
2025-04-16 17:32:12 +0500

    fix: fix ci cd and fixed ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,23 @@
 module.exports = {
   apps: [
     {
-      name: 'prod-subdomain-soffcrm-uz-3132',
-      script: 'npm',
-      args: 'start',
-    },
-    {
       name: 'test-subdomain-soffcrm-uz-3132',
       script: 'npm',
-      args: 'start',
-    },
-  ],
+      args: 'run start',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      },
+      output: './logs/out.log',
+      error: './logs/error.log',
+      log: './logs/combined.log',
+      time: true
+    }
+  ]
 }
