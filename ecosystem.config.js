fe69fa7b2ae69cdd6ab3092d0d0efe6d142174ab
warnigo <abubakirshavkatov0012@gmail.com>
2025-04-17 10:13:34 +0500

    chore: turn back ci/cd

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,18 +4,6 @@ module.exports = {
       name: "test-subdomain-soffcrm-uz-3132",
       script: "npm",
       args: "run start",
-      interpreter: "/root/.nvm/versions/node/v20.10.0/bin/node",
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: "1G",
-      env: {
-        NODE_ENV: "production"
-      },
-      output: "./logs/out.log",
-      error: "./logs/error.log",
-      log: "./logs/combined.log",
-      time: true
     }
   ]
 };
