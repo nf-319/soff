5d2798af4359d95d5c0649b82b071b6b0033f67e
warnigo <abubakirshavkatov0012@gmail.com>
2025-05-07 16:14:23 +0500

    fix: some changes ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,10 @@ module.exports = {
   apps: [
     {
       name: "test-subdomain-soffcrm-uz-3131",
-      script: "npm",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
-      instances: "max",
-      exec_mode: "cluster",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
         PORT: 3131
@@ -18,10 +18,10 @@ module.exports = {
     },
     {
       name: "test-subdomain-soffcrm-uz-3131-new",
-      script: "npm",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
-      instances: "max",
-      exec_mode: "cluster",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
         PORT: 3130
@@ -34,10 +34,10 @@ module.exports = {
     },
     {
       name: "prod-subdomain-soffcrm-uz-3132",
-      script: "npm",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
-      instances: "max",
-      exec_mode: "cluster",
+      instances: 1,
+      exec_mode: "fork",
       env: {
         NODE_ENV: "production",
         PORT: 3132
