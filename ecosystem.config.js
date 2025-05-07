c6226fa7c62afb13b6bba7da6497c7639ae51f31
warnigo <abubakirshavkatov0012@gmail.com>
2025-05-07 11:58:26 +0500

    fix: fix CI/CD and ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,11 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3131
       },
-      max_memory_restart: "500M"
+      max_memory_restart: "500M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "./logs/error.log",
+      out_file: "./logs/out.log",
+      merge_logs: true
     },
     {
       name: "test-subdomain-soffcrm-uz-3131-new",
@@ -22,7 +26,11 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3130
       },
-      max_memory_restart: "500M"
+      max_memory_restart: "500M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "./logs/error-new.log",
+      out_file: "./logs/out-new.log",
+      merge_logs: true
     },
     {
       name: "prod-subdomain-soffcrm-uz-3132",
@@ -34,7 +42,11 @@ module.exports = {
         NODE_ENV: "production",
         PORT: 3132
       },
-      max_memory_restart: "500M"
+      max_memory_restart: "500M",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+      error_file: "./logs/error-prod.log",
+      out_file: "./logs/out-prod.log",
+      merge_logs: true
     }
   ]
 };
