2169d6ee47e1fb992648d93f25c2c16ba63358f9
warnigo <abubakirshavkatov0012@gmail.com>
2025-05-07 11:39:38 +0500

    fix: fix CI/CD and ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,40 @@
 module.exports = {
   apps: [
     {
-      name: "test-subdomain-soffcrm-uz-3132",
-      script: "npm",
-      args: "run start:test",
+      name: "test-subdomain-soffcrm-uz-3131",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3131
+      },
+      max_memory_restart: "500M"
+    },
+    {
+      name: "test-subdomain-soffcrm-uz-3131-new",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3130
+      },
+      max_memory_restart: "500M"
     },
     {
       name: "prod-subdomain-soffcrm-uz-3132",
-      script: "npm",
-      args: "run start",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3132
+      },
+      max_memory_restart: "500M"
     }
   ]
 };
