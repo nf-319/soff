a4b3ab026d3e687da0c5c2857299a77a44dda565
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-05-08 19:18:22 +0500

    feat/mentor-profile card

--- src/widgets/MentorOverview/MentorOverview.tsx
+++ src/widgets/MentorOverview/MentorOverview.tsx
@@ -1,13 +1,62 @@
-import { Box } from "@mui/material"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 import classnames from 'classnames/bind'
-
 import style from './MentorOverview.module.scss'
-
+import { } from "react-bootstrap"
+import { Box } from '@mui/material';
+import { MentorInfo } from './constants'
+import { Pencil, Phone } from 'lucide-react';
+import Image from 'next/image';
 const cn = classnames.bind(style)
 
 export const MentorOverview = () => {
     return (
-        <Box component='section' className={cn("prent__list")}>mentor</Box>
+        <Box component='section' className={cn("mentor_overview")}>
+            <Card className={cn("mentor_overview__card")} sx={{ boxShadow: 'none', maxWidth: 345 }}>
+                <CardContent className={cn("mentor_overview__card-content")}>
+                    <Box className={cn("mentor_overview__card-content-titleWrap")}>
+                        <Box className={cn("mentor_overview__card-content-titleWrap-titleBox")}>
+                            <Typography variant='h6' color={'black'} className={cn("mentor_overview__card-content-titleWrap-titleBox-title")}>
+                                Mening malumotlarim
+                            </Typography>
+                            <Pencil size={20} className='mentor_overview__card-content-titleWrap-titleBox-editor' />
+                        </Box>
+                        {/* <img className={cn("mentor_overview__card-content-titleWrap-avatar")} src="https://picsum.photos/id/237/300/300" alt="" /> */}
+                        <Image className={cn("mentor_overview__card-content-titleWrap-avatar")} src={"https://picsum.photos/id/237/300/300"} width={120} height={120} alt='avatar' />
+                    </Box>
+                    <Box className={cn("mentor_overview__card-content-body")}>
+                        {
+                            MentorInfo.map((item, index) => {
+                                return (
+                                    <Box key={index} className={cn("mentor_overview__card-content-body-item")}>
+                                        <item.icon size={20} />
+                                        <Box>
+                                            <Typography className={cn("mentor_overview__card-content-body-title")}>
+                                                {item.title}
+                                            </Typography>
+                                            <Typography color={'black'} className={cn("mentor_overview__card-content-body-title")}>
+                                                {item.value}
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                );
+                            })
+                        }
+
+                    </Box>
+                    <Typography marginY={2.5} className={cn("mentor_overview__card-content-contact")}>
+                        Kontakt
+                    </Typography>
+                    <Box className={cn("mentor_overview__card-content-contactbox")}>
+                        <Phone />
+                        <Typography color={'black'} className={cn("mentor_overview__card-content-body-title")}>
+                            +998 90 123 45 67
+                        </Typography>
+                    </Box>
+                </CardContent>
+            </Card>
+        </Box >
     )
 }
 
---
+++ src/widgets/MentorOverview/constants.ts
@@ -0,0 +1,24 @@
+import { BookCopy, Calendar, MapPin, Users } from 'lucide-react'
+
+export const MentorInfo = [
+  {
+    title: "Ro'yxatdan o'tgan sana",
+    icon: Calendar,
+    value: '12.05.2023'
+  },
+  {
+    title: 'Filial',
+    icon: MapPin,
+    value: 'Chilonzor filiali'
+  },
+  {
+    title: "O'qitayotgan kurslar soni",
+    icon: BookCopy,
+    value: '4'
+  },
+  {
+    title: 'Faol guruhlar soni',
+    icon: Users,
+    value: '4'
+  }
+]
