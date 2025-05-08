dafef5eea28924048cacb08aa1dbf654b48acf74
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-05-08 16:16:24 +0500

    fix: start metor age

---
+++ src/widgets/MentorOverview/MentorOverview.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material"
+import classnames from 'classnames/bind'
+
+export const MentorOverview = () => {
+    return (
+        <Box component='section'>mentor</Box>
+    )
+}
+
+MentorOverview.displayName = 'MentorOverview'
