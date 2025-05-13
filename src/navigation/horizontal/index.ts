18bd8181db7c25a511e1764da9bbf581b625449d
Otabek0880 <hdildora42@gmail.com>
2025-05-13 18:27:18 +0500

    fix payments reports

--- src/navigation/horizontal/index.ts
+++ src/navigation/horizontal/index.ts
@@ -115,10 +115,10 @@ const Navigation = (t: any): HorizontalNavItemsType => {
       title: t('Hisobotlar'),
       icon: 'tabler:report',
       children: [
-        {
-          title: "To'lovlar hisoboti",
-          path: '/reports/payment'
-        },
+        // {
+        //   title: "To'lovlar hisoboti",
+        //   path: '/reports/payment'
+        // },
         {
           title: "O'quvchilar to'lovi",
           path: '/reports/student-payment'
