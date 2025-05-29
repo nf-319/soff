675543a82a64c3264c02d8c87fae6e0993375471
AnvarMirsaatov <anvarmirsaatov6358@gmail.com>
2025-05-29 15:55:11 +0500

    no changes

--- src/views/apps/dashboard/DashboardNumbers.tsx
+++ src/views/apps/dashboard/DashboardNumbers.tsx
@@ -16,10 +16,10 @@ const DashboardNumbers = () => {
           <Grid item xs={12}>
             <FinanceAllNumber />
           </Grid>
-          <Grid item xs={12} sm={12} md={4}>
+          <Grid item xs={12} sm={12} md={6}>
             <StatsPaymentMethods />
           </Grid>
-          <Grid item xs={12} md={8} mb={10}>
+          <Grid item xs={12} md={6}>
             <CardStatisticsLiveVisitors />
           </Grid>
         </Grid>
